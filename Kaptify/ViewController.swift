21c1514a5ccef5e1a19f977a3acdf2c1b66857ab
SKapal <sahilkapal@cmail.carleton.ca>
2018-06-24 19:26:10 -0400

    Initial Commit

---
+++ Kaptify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaptify
+//
+//  Created by Sahil Kapal on 2018-06-24.
+//  Copyright © 2018 Sahil Kapal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
