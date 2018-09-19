498815106a64bfa76b054ac2f6a50dc5847ceb30
SKapal <sahilkapal@cmail.carleton.ca>
2018-09-19 17:59:00 -0400

    add preliminary 'recent activity model'

---
+++ Kaptify/Screens/Profile/Models/RecentActivityModel.swift
@@ -0,0 +1,16 @@
+//
+//  RecentActivityModel.swift
+//  Kaptify
+//
+//  Created by Sahil Kapal on 2018-09-19.
+//  Copyright © 2018 Sahil Kapal. All rights reserved.
+//
+
+import Foundation
+
+struct RecentActivity {
+    let title: String
+    let url: URL
+    
+    
+}
